c8ac9fdeeedb1bf773db993c14045a28969b8cf5
Nopnapat Norasri <neuw@Nopnapats-MacBook-Pro.local>
2023-12-12 15:05:21 +0700

    style(common/screen): under construction

--- lib/core/common/screens/page_under_construction.dart
+++ lib/core/common/screens/page_under_construction.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 import 'package:tdd_bloc/core/common/widgets/gradient_background_widget.dart';
 import 'package:tdd_bloc/core/constants/app_asset.dart';
+import 'package:tdd_bloc/core/constants/app_color.dart';
 
 class PageUnderConstruction extends StatelessWidget {
   const PageUnderConstruction({super.key});
@@ -9,6 +10,9 @@ class PageUnderConstruction extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        appBar: AppBar(
+          backgroundColor: AppColors.kColorWhite,
+        ),
         body: GradientBackgroundWidget(
             image: AppAssets.kImageBackground,
             child: Center(
